76d103ab31cdd44e2bbc38893cdf5795d10594dc
Antoni Zasada <zsd.antoni@gmail.com>
2025-05-23 13:26:42 +0200

    custom controls

---
+++ WpfCustomControls/CustomControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCustomControls
+{
+    /// <summary>
+    /// Interaction logic for ClearableTextBox.xaml
+    /// </summary>
+    public partial class ClearableTextBox : UserControl
+    {
+        private string _placeholder;
+
+        public string Placeholder
+        {
+            get => _placeholder;
+            set
+            {
+                _placeholder = value;
+                tblkPlaceholder.Text = _placeholder;
+            }
+        }
+
+        public ClearableTextBox()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            tboxTextInput.Clear();
+        }
+
+        private void tboxTextInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tboxTextInput.Text))
+            {
+                tblkPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                tblkPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
---
+++ WpfCustomControls/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCustomControls;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    private string _someText;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public string SomeText
+    {
+        get => _someText;
+        set
+        {
+            _someText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MainWindow()
+    {
+        DataContext = this;
+        InitializeComponent();
+    }
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
